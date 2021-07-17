e8fc5c761f9f91feb7edd1f0297ea68a04461d78
hanbae.seo <hanbae.seo@linecorp.com>
2021-07-17 23:23:49 +0900

    for kubeflow 1.3

--- kubeflow/fairing/notebook/notebook_util.py
+++ kubeflow/fairing/notebook/notebook_util.py
@@ -2,16 +2,21 @@ import json
 import os.path
 import ipykernel
 import re
-from notebook.notebookapp import list_running_servers
 import requests
+import importlib
+from notebook.notebookapp import list_running_servers
 from requests.compat import urljoin
-
 
 def get_notebook_name(): #pylint:disable=inconsistent-return-statements
     """Return the full path of the jupyter notebook. """
     kernel_id = re.search('kernel-(.*).json',
                           ipykernel.connect.get_connection_file()).group(1)
-    servers = list_running_servers()
+    jupyter_server_spec = importlib.util.find_spec("jupyter_server")
+    if jupyter_server_spec is not None:
+        from jupyter_server import serverapp
+        servers = serverapp.list_running_servers()
+    else:
+        servers = list_running_servers()
     for ss in servers:
         response = requests.get(urljoin(ss['url'], 'api/sessions'),
                                 params={'token': ss.get('token', '')})
